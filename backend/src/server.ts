8c81699dff03fdbc2d0698545218aa1ce3aaaf50
HenriqueSagawa <henriquetutomusagawa@gmail.com>
2026-05-24 00:29:44 -0300

    feat: add Prisma client connection and configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 import app from "./app";
 import { env } from "./config/env";
+import { prisma } from "./config/prisma";
 
 async function bootstrap() {
   try {
+    await prisma.$connect();
+    console.log("Conexão com o banco de dados estabelecida com sucesso.");
+
     app.listen(env.PORT, () => {
       console.log(`Servidor rodando na porta ${env.PORT}...`);
     });
